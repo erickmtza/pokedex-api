61c3d0d6af713281f4f8ddc78d478b2bc45df582
Erick Martinez <erick.martinez2013@gmail.com>
2019-03-30 22:38:55 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychich`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+});
